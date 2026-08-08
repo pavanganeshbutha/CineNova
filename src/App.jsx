6959a513a06555fc3e444ec4fa65d7b9815ea116
pavanganeshbutha <pavanganeshbutha@gmail.com>
2026-08-08 17:06:54 +0530

    practise

--- src/App.jsx
+++ src/App.jsx
@@ -1,121 +1,54 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from './assets/vite.svg';
-import heroImg from './assets/hero.png';
-import './App.css';
+import { useState, useEffect } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0);
+const App = () => {
+  return (
+    <div className="flex min-h-screen flex-col">
+      <header className="min-h-20 flex items-center justify-center text-4xl font-bold bg-purple-400">
+        <h1>CineNova</h1>
+      </header>
+      <main className="flex-1">
+        <Banner />
+      </main>
+      <footer>footer</footer>
+    </div>
+  );
+};
+
+function Banner() {
+  const [movieDetails, setMovieDetails] = useState({});
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization:
+        'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5ZTg5NmNmMGU3M2I4ODJlMjVlMjU0MDYwYTVjNzAyNyIsIm5iZiI6MTc2ODA3MDQ0MS43NDEsInN1YiI6IjY5NjI5ZDI5ZDA3YmQ3MTAxNGQxNGQwZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.A6g_6OcmaBJyTBcY-SvQj2lNzGWA9SbdKm3_zbLE5Bg',
+    },
+  };
+
+  useEffect(function () {
+    async function fetchMovieDetails() {
+      const response = await fetch(
+        `https://api.themoviedb.org/3/movie/27205?language=en-US`,
+        options,
+      );
+      const data = await response.json();
+      setMovieDetails(data);
+      console.log(data);
+    }
+
+    fetchMovieDetails();
+  }, []);
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+    <section className="min-h-96 bg-black bg-[url(https://image.tmdb.org/t/p/w1280/8ZTVqvKDQ8emSGUEMjsS4yHAwrp.jpg)] bg-center bg-no-repeat bg-cover p-4 bg-blend-luminosity">
+      <h1 className="text-6xl font-extrabold text-white">
+        {movieDetails.title}
+      </h1>
+      <h4 className="text-4xl font-semibold text-white mt-5">
+        {movieDetails.tagline}
+      </h4>
+      <p className="text-2xl text-white mt-5">{movieDetails.overview}</p>
+    </section>
   );
 }
 
